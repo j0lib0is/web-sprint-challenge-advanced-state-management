eababbb082d8604c1fdfee11db891191a4ab4194
jacobjolibois <jacobjolibois@hey.com>
2022-01-20 21:39:36 -0600

    set up my action and reducer files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,32 @@
 import axios from 'axios';
 
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
+export const ADD_SMURF = 'ADD_SMURF';
+export const SET_ERROR = 'SET_ERROR';
+
+export const fetchStart = () => {
+	return({type: FETCH_START});
+}
+
+export const fetchSuccess = (smurfs) => {
+	return({type: FETCH_SUCCESS, payload: smurfs});
+}
+
+export const fetchFail = (error) => {
+	return({type: FETCH_FAIL, payload: error});
+}
+
+export const addSmurf = (smurf) => {
+	return({type: ADD_SMURF, payload: smurf});
+}
+
+export const setError = (error) => {
+	return({type: SET_ERROR, payload: error});
+}
+
 //Task List:
-//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
-//2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
-//3. Add a standard action that allows us to set the value of the error message slice of state.
+//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retrieve smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+//2. ✅ Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+//3. ✅ Add a standard action that allows us to set the value of the error message slice of state.
