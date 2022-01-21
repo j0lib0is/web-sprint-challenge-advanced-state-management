d65d702b44a15181bde20808c9f1c218efca9823
jacobjolibois <jacobjolibois@hey.com>
2022-01-20 21:44:29 -0600

    created a thunk action to fetch the Smurf list

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,20 @@ export const FETCH_FAIL = 'FETCH_FAIL';
 export const ADD_SMURF = 'ADD_SMURF';
 export const SET_ERROR = 'SET_ERROR';
 
+export const fetchSmurfs = () => {
+	return (dispatch) => {
+		dispatch({type: FETCH_START});
+		axios.get('http://localhost:3333/smurfs')
+			.then(resp => {
+				console.log(resp);
+				// dispatch({type: FETCH_SUCCESS, payload: resp});
+			})
+			.catch(err => {
+				dispatch({type: FETCH_FAIL, payload: err});
+			})
+	}
+}
+
 export const fetchStart = () => {
 	return({type: FETCH_START});
 }
@@ -27,6 +41,6 @@ export const setError = (error) => {
 }
 
 //Task List:
-//1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retrieve smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+//1. ✅ Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retrieve smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. ✅ Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. ✅ Add a standard action that allows us to set the value of the error message slice of state.
